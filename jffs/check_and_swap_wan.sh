4465884cbe7379f4c2d0f956f3ba59c0c4932dd1
ADA <ada3000@gmail.com>
2020-11-10 22:20:18 +0300

    Update check_and_swap_wan.sh

--- jffs/check_and_swap_wan.sh
+++ jffs/check_and_swap_wan.sh
@@ -1,10 +1,16 @@
+# Check cron execution
 echo Start inet check >> /tmp/inet_check.txt
 
+# In my case:
+# First WAN is ppp0 - optic lan provider
+# Second WAN is eth3 - usb modem
+
+# Gateway for my first WAN
 test_server=10.11.51.1
 result=`ping $test_server -c 4 | grep "100% packet loss"`
 if [ -z "$result"]
 then
-	echo Turn on Domolad                                                                                                                                        
+	echo Turn on First WAN (ppp0), turn off second (eth3)                                                                                                                                        
         ifconfig ppp0 up; ifconfig eth3 down         
 else
 	echo Turn on Ele2
